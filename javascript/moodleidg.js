61363ce3c01a19dc1006df9ad50dae85af2e9aed
Fábio Santos <f4bio.sa@gmail.com>
2017-11-09 09:39:41 -0400

    Salvo a ativação do alto contraste em um cookie.

--- javascript/moodleidg.js
+++ javascript/moodleidg.js
@@ -1,4 +1,70 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function contraste(evento){
+    var contraste = getCookie("contraste");
+
+    if (contraste!="") {
+        if(evento=="click") {
+            setCookie("contraste", "", 365);
+            $('div.layout').removeClass('contraste');
+            $('#menu-nav').addClass('navbar-light bg-faded');
+            $('#menu-nav').removeClass('navbar-dark bg-dark');
+        } else {
+            $('div.layout').addClass('contraste');
+            $('#menu-nav').addClass('navbar-dark bg-dark');
+        }
+        /* layout_classes = $.cookie('layout_classes');
+        layout_classes = layout_classes.replace('contraste', ''); */
+        // $.cookie('layout_classes', layout_classes );
+
+    } else {
+        if (evento == "click") {
+            setCookie("contraste", "ligado", 365);
+            $('div.layout').addClass('contraste');
+            $('#menu-nav').removeClass('navbar-light bg-faded');
+            $('#menu-nav').addClass('navbar-dark bg-dark');
+        } else {
+            $('div.layout').removeClass('contraste');
+            $('#menu-nav').removeClass('navbar-dark bg-dark');
+        }
+        // layout_classes = $.cookie('layout_classes');
+        // if( layout_classes != 'undefined' )
+        //     layout_classes = layout_classes + ' contraste';
+        // else
+        //    layout_classes = 'contraste';
+        // $.cookie('layout_classes', layout_classes );
+
+    }
+}
+
+function init(){
+    contraste("");
+}
+
 $(document).ready(function () {
+
+    init();
+
     // Custom
     var stickyToggle = function (sticky, stickyWrapper, scrollElement, navdrawer) {
         var stickyHeight = sticky.outerHeight();
@@ -35,25 +101,10 @@ $(document).ready(function () {
 
     // Alto contrast
     $('a.toggle-contraste').click(function () {
-        if (!$('div.layout').hasClass('contraste')) {
-            $('div.layout').addClass('contraste');
-            $('#menu-nav').removeClass('navbar-light bg-faded');
-            $('#menu-nav').addClass('navbar-dark bg-dark');
-            // layout_classes = $.cookie('layout_classes');
-            // if( layout_classes != 'undefined' )
-            //     layout_classes = layout_classes + ' contraste';
-            // else
-            //    layout_classes = 'contraste';
-            // $.cookie('layout_classes', layout_classes );
-        } else {
-            $('div.layout').removeClass('contraste');
-            $('#menu-nav').addClass('navbar-light bg-faded');
-            $('#menu-nav').removeClass('navbar-dark bg-dark');
-            /* layout_classes = $.cookie('layout_classes');
-            layout_classes = layout_classes.replace('contraste', ''); */
-            // $.cookie('layout_classes', layout_classes );
-        }
+        contraste("click")
     });
+
+
 
     //fim acao botao de alto contraste
 

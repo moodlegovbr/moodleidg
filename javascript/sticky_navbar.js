80a1d6ec8de21b29843d1749d89d3ad3230ecf09
Fábio Santos <f4bio.sa@gmail.com>
2019-02-12 16:26:19 -0400

    Ajuste do massage-drawer

--- javascript/sticky_navbar.js
+++ javascript/sticky_navbar.js
@@ -11,17 +11,19 @@
 
 $(document).ready(function() {
   // Custom function which toggles between sticky class (is-sticky)
-  var stickyToggle = function(sticky, stickyWrapper, scrollElement, navdrawer) {
+  var stickyToggle = function(sticky, stickyWrapper, scrollElement, navdrawer, messagedrawer) {
     //var stickyHeight = sticky.outerHeight();
     var stickyTop = stickyWrapper.offset().top;
     if (scrollElement.scrollTop() >= stickyTop-35){
       //stickyWrapper.height(stickyHeight);
       sticky.addClass("is-sticky");
       navdrawer.addClass("nav-drawer-sticky");
+      messagedrawer.addClass("message-drawer-sticky");
     }
     else{
       sticky.removeClass("is-sticky");
       navdrawer.removeClass("nav-drawer-sticky");
+      messagedrawer.removeClass("message-drawer-sticky");
       stickyWrapper.height('auto');
     }
   };
@@ -31,15 +33,16 @@ $(document).ready(function() {
     var sticky = $(this);
     var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
     var navdrawer = $('#nav-drawer');
+    var messagedrawer = $('.message-drawer');
     sticky.before(stickyWrapper);
     sticky.addClass('sticky');
     
     // Scroll & resize events
     $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
-      stickyToggle(sticky, stickyWrapper, $(this), navdrawer);
+      stickyToggle(sticky, stickyWrapper, $(this), navdrawer, messagedrawer);
     });
     
     // On page load
-    stickyToggle(sticky, stickyWrapper, $(window), navdrawer);
+    stickyToggle(sticky, stickyWrapper, $(window), navdrawer, messagedrawer);
   });
 });
